00f0b0d345cc8220cee13d964a79acb677963eed
peterspenler <peterspenler@gmail.com>
2019-05-28 16:04:45 -0400

    updated navbar icons to highlight on select

--- pizza-topper/src/NavBar.js
+++ pizza-topper/src/NavBar.js
@@ -20,14 +20,21 @@ class NavBar extends React.Component {
 		let selectClass = ""
 		let pizzaClass = ""
 
+		let userIcon = require('./graphics/icon_user_black.png')
+		let selectIcon = require('./graphics/icon_select_black.png')
+		let pizzaIcon = require('./graphics/icon_pizza_black.png')
+
 		if(this.props.page === "User" || this.props.page === "/"){
 			userClass = "selected"
+			userIcon = require('./graphics/icon_user_blue.png')
 		}
 		if(this.props.page === "Select"){
 			selectClass = "selected"
+			selectIcon = require('./graphics/icon_select_blue.png')
 		}
 		if(this.props.page === "Pizza"){
 			pizzaClass = "selected"
+			pizzaIcon = require('./graphics/icon_pizza_blue.png')
 		}
 
 
@@ -36,17 +43,17 @@ class NavBar extends React.Component {
 				<p>Pizza Topper</p>
 				<Link to='/users'>
 					<button id="user-btn" className={userClass} onClick={e => this.handleClick(e, "user")}>
-						<img className="icon" alt="Users" src={require('./graphics/user-icon.png')}/>
+						<img className="icon" alt="Users" src={userIcon}/>
 					</button>
 				</Link>
 				<Link to='/select'>
 					<button id="select-btn" className={selectClass} onClick={e => this.handleClick(e, "select")}>
-						<img className="icon" alt="Select toppings" src={require('./graphics/select-icon.png')}/>
+						<img className="icon" alt="Select toppings" src={selectIcon}/>
 					</button>
 				</Link>
 				<Link to='/pizza'>
 					<button id="pizza-btn" className={pizzaClass} onClick={e => this.handleClick(e, "pizza")}>
-						<img className="icon" alt="Pizzas" src={require('./graphics/pizza-icon.png')}/>
+						<img className="icon" alt="Pizzas" src={pizzaIcon}/>
 					</button>
 				</Link>
 			</div>
